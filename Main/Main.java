3aac1ee6eadce693786d6a8976e0578304f0d47e
DANGLONG <c210105@ccg.ac.jp>
2021-10-18 11:58:37 +0900

    計算機session 1.0

---
+++ Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("皆さんこんにちは");
+        System.out.println("計算機を作りましょうね");
+
+        int a = 1;
+        int b = 20;
+
+        System.out.println("a + b = " + (a + b));
+
+    }
+}
